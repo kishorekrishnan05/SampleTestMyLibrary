6a6d56ebca10b0082ca8ff114473072298692aaf
Kishore <Kishore.krish05@gmail.com>
2022-08-17 19:08:33 +0530

    test

--- Sources/TestMyLibrary/TestMyLibrary.swift
+++ Sources/TestMyLibrary/TestMyLibrary.swift
@@ -1,6 +1,8 @@
-public struct TestMyLibrary {
-    public private(set) var text = "Hello, World!"
 
-    public init() {
+import UIKit
+public struct TestMyLibrary {
+    static public func createViewController() -> SampleViewController {
+        let storyboard = UIStoryboard(name: "SampleViewController", bundle: Bundle.module)
+        return storyboard.instantiateInitialViewController() as! SampleViewController
     }
 }
