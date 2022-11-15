efc7b50870286174b01f8cd3d81c24ba87be0468
Kishore <39756075+kishorekrishnan05@users.noreply.github.com>
2022-11-15 13:35:40 +0530

    tteddhf

--- Sources/TestMyLibrary/SampleViewController.swift
+++ Sources/TestMyLibrary/SampleViewController.swift
@@ -712,6 +712,7 @@ extension SampleViewController{
             button.frame = CGRect(x: leadingValue, y: CGFloat(36), width: value.seatSize ?? 0, height: 16)
             
             leadingValue = leadingValue  + CGFloat(8) + (value.seatSize ?? 0)
+            viewHeader.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 65)
             viewHeader.addSubview(button)
         }
     }
