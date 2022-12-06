a6e8f30ab9e653bfdbba939694b90326be18d1f3
Kishore <39756075+kishorekrishnan05@users.noreply.github.com>
2022-12-06 22:46:39 +0530

    test

--- Sources/TestMyLibrary/TestMyLibrary.swift
+++ Sources/TestMyLibrary/TestMyLibrary.swift
@@ -1,8 +1,8 @@
 
-import UIKit
+/*import UIKit
 public struct TestMyLibrary {
     static public func createViewController() -> SampleViewController {
         let storyboard = UIStoryboard(name: "Main", bundle: Bundle.module)
         return storyboard.instantiateInitialViewController() as! SampleViewController
     }
-}
+}*/
